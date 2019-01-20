13ca88b4e39b9d85cbf9fa8a28084063e1f60a9a
SunDev774 <sun__sun@windowslive.com>
2019-01-20 11:52:03 +0700

    InClass Week02 Intro01_UDPClient_Server

---
+++ 01-Intro/UDPClient/Program.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Net;
+using System.Net.Sockets;
+using System.Text;
+
+
+namespace UDPClient
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+           string server = "10.1.116.239"; // loop back address
+           int servPort = 5000;
+
+           string message = "Tanawat 5808657";
+
+           byte[] sendPacket = Encoding.ASCII.GetBytes(message);
+
+           UdpClient client = new UdpClient();
+
+           try
+           {
+               client.Send(sendPacket,sendPacket.Length,server,servPort);
+           }
+           catch(SocketException e)
+           {
+               Console.WriteLine(e.ErrorCode+""+e.Message);
+           }
+        }
+    }
+}
---
+++ 01-Intro/UDP_Server/Program.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Net;
+using System.Net.Sockets;
+using System.Text;
+
+namespace UDP_Server
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int servPort = 5000;
+            UdpClient client = null;
+
+            client = new UdpClient(servPort);
+
+            IPEndPoint remoteIPEndPoint = new IPEndPoint(IPAddress.Any,0);
+
+            for(;;)
+            {
+                try
+                {
+                    byte[] byteBuffer = client.Receive(ref remoteIPEndPoint);
+
+                    string recvMessage = Encoding.ASCII.GetString(byteBuffer,0,byteBuffer.Length);
+                    //string sendBack = Encoding.ASCII.GetString();
+                    Console.WriteLine(recvMessage);
+                }
+                catch(SocketException e)
+                {
+                    Console.WriteLine(e.ErrorCode+" "+e.Message);
+                    
+                }
+            }
+        }
+    }
+}
